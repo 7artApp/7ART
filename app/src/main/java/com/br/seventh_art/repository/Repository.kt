d5922db271b7b3fff5010690436300c039e256d6
Lais Rolim Yonaha <laisrolimyonaha@users.noreply.github.com>
2021-04-05 21:39:14 -0300

    commit

---
+++ app/src/main/java/com/br/seventh_art/repository/Repository.kt
@@ -0,0 +1,15 @@
+package com.br.seventh_art.repository
+
+import RetrofitInit
+import com.br.seventh_art.network.EndPointApi
+
+class Repository{
+
+    private var url = "https://api.themoviedb.org/"
+    private var service = EndPointApi::class
+    private var chave = "d00ab27062c01a80c4f6a7cefd66a6a5"
+
+    private val serviceApp = RetrofitInit(url).create(service)
+
+//    suspend fun getCharacterService(): CharacterResponse = serviceApp.getResponseCharacter()
+}
