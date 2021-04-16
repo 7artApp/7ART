af347a659dbe7a823f93e12c816de9d3630881ef
Lais Rolim Yonaha <laisrolimyonaha@users.noreply.github.com>
2021-04-15 21:12:25 -0300

    Repositório Movie/Serie

--- app/src/main/java/com/br/seventh_art/repository/Repository.kt
+++ app/src/main/java/com/br/seventh_art/repository/Repository.kt
@@ -1,7 +1,8 @@
 package com.br.seventh_art.repository
 
 import RetrofitInit
-import com.br.seventh_art.data.model.genre.MovieGenreResponse
+import com.br.seventh_art.data.model.genreMovie.MovieGenreResponse
+import com.br.seventh_art.data.model.genreSerie.SerieGenreResponse
 import com.br.seventh_art.network.EndPointApi
 
 const val API_KEY = "d00ab27062c01a80c4f6a7cefd66a6a5"
@@ -12,6 +13,6 @@ class Repository{
 
     private val serviceApp = RetrofitInit(url).create(service)
 
-    suspend fun getMovieGenreServie():MovieGenreResponse = serviceApp.getAllGenre(API_KEY)
-
+    suspend fun getMovieGenreService():MovieGenreResponse = serviceApp.getAllGenreMovie(API_KEY)
+    suspend fun getSerieGenreService(): SerieGenreResponse = serviceApp.getAllGenreSerie(API_KEY)
 }
