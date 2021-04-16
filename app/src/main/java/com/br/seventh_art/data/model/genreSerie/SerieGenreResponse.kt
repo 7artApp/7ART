e06ae261b12f458f5ad96151a981351ad264b900
CaioCrevelaro <caio.m.b.crevelaro@gmail.com>
2021-04-16 11:16:53 -0300

    ViewModel das telas de Gêneros concluídos

--- app/src/main/java/com/br/seventh_art/data/model/genreSerie/SerieGenreResponse.kt
+++ app/src/main/java/com/br/seventh_art/data/model/genreSerie/SerieGenreResponse.kt
@@ -1,5 +1,5 @@
 package com.br.seventh_art.data.model.genreSerie
 
 data class SerieGenreResponse(
-    val genres: List<Genre>
+    val genres: List<SerieGenre>
 )
--- app/src/main/java/com/br/seventh_art/viewmodel/MovieGenreViewModel.kt
+++ app/src/main/java/com/br/seventh_art/viewmodel/MovieGenreViewModel.kt
@@ -1,6 +1,24 @@
 package com.br.seventh_art.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.br.seventh_art.data.model.genreMovie.MovieGenre
+import com.br.seventh_art.repository.Repository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MovieGenreViewModel: ViewModel() {
+
+    val listMutableMovieGenre = MutableLiveData<List<MovieGenre>>()
+    private val repository = Repository()
+
+    fun getAllMovieGenre() = CoroutineScope(Dispatchers.IO)
+        .launch {
+            repository.getMovieGenreService()
+                .let { movieGenreResponse ->
+                    listMutableMovieGenre
+                        .postValue(movieGenreResponse.genres)
+                }
+        }
 }
