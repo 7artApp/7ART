ac770664636917e0f88c3272925aaa73ed9b0840
RetsDev <renatocmw@gmail.com>
2021-05-14 16:25:30 -0300

    atualiza view holder

--- app/src/main/java/com/br/seventh_art/view/recommendations/movies/adapter/viewholder/MoviesRecViewHolder.kt
+++ app/src/main/java/com/br/seventh_art/view/recommendations/movies/adapter/viewholder/MoviesRecViewHolder.kt
@@ -2,7 +2,6 @@ package com.br.seventh_art.view.recommendations.movies.adapter.viewholder
 
 import android.view.View
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.br.seventh_art.R
 
@@ -11,9 +10,5 @@ class MoviesRecViewHolder(
 ):  RecyclerView.ViewHolder(view) {
 
     val movie_image by lazy { itemView.findViewById<ImageView>(R.id.movie_image) }
-    val movie_title by lazy { itemView.findViewById<TextView>(R.id.movie_title)}
-    val movie_release  by lazy { itemView.findViewById<TextView>(R.id.movie_release) }
-    val movie_score  by lazy { itemView.findViewById<TextView>(R.id.movie_score) }
-    val movie_disclaimer by lazy { itemView.findViewById<TextView>(R.id.movie_disclaimer)}
 
 }
